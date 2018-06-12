961b5cedb06698b1cdded867832a51bf2fdeea75
kevin <kevinjobrien420@gmail.com>
2018-06-11 23:11:59 -0500

    181 App Number 15 - Part1 - Using Control statements and Arrays Together //Coding

--- app/src/main/java/com/example/kevin/usingcontrolstatementsandarraystogether/MainActivity.java
+++ app/src/main/java/com/example/kevin/usingcontrolstatementsandarraystogether/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.usingcontrolstatementsandarraystogether;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtFiveStarsStudent = (TextView)findViewById(R.id.txtFiveStarsStudent);
+        TextView txtFourStarsStudent = (TextView)findViewById(R.id.txtFourStarsStudent);
+        TextView txtThreeStarsStudent = (TextView)findViewById(R.id.txtThreeStarsStudent);
+        TextView txtTwoStarsStudent = (TextView)findViewById(R.id.txtTwoStarsStudent);
+        TextView txtOneStarsStudent = (TextView)findViewById(R.id.txtOneStarsStudent);
     }
 }
